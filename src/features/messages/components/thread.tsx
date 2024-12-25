cbd3ad1504e6caa5981418e9382b53e7c058ff1b
Nikita <ragerage333@gmail.com>
2024-12-25 12:38:51 +0300

    Fixed errors 2

--- src/features/messages/components/thread.tsx
+++ src/features/messages/components/thread.tsx
@@ -87,7 +87,7 @@ export const Thread = ({ messageId, onClose }: ThreadProps) => {
       };
 
       if (image) {
-        const url = await generateUploadUrl({}, { throwError: true });
+        const url = await generateUploadUrl({ throwError: true });
 
         if (!url) {
           throw new Error("URL not found");
